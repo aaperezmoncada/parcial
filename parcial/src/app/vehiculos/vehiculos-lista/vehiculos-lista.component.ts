d50e4a7280f0624a8c3677bf645e65287c629832
aaperezmoncada <aa.perezm12@uniandes.edu.co>
2022-11-05 16:36:27 -0500

    Adicionar cálculo total vehículos por marca

--- parcial/src/app/vehiculos/vehiculos-lista/vehiculos-lista.component.ts
+++ parcial/src/app/vehiculos/vehiculos-lista/vehiculos-lista.component.ts
@@ -10,13 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class VehiculosListaComponent implements OnInit {
 
   vehiculos: Vehiculo[] = []
+  marcas = new Map()
 
   constructor(private vehiculosService: VehiculosService) { }
 
   getVehiculos() {
     this.vehiculosService.getVehiculos().subscribe(data => {
       this.vehiculos = data
+      this.getTotalizadorMarcas()
     })
+  }
+
+  getTotalizadorMarcas() {
+    this.vehiculos.forEach(element => {
+      let total = 0;
+      if (this.marcas.has(element.marca)) {
+        total = this.marcas.get(element.marca)
+      }
+      total += 1
+      this.marcas.set(element.marca, total)
+    });
   }
 
   ngOnInit(): void {
