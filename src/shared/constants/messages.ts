7bd74639f152ff921702a26f9f1a77dbeae34be6
Alvaro Perez <aapm12@hotmail.com>
2024-09-14 12:26:39 -0500

    Lógica Inicial Ciudad Supermercado

--- src/shared/constants/messages.ts
+++ src/shared/constants/messages.ts
@@ -2,6 +2,6 @@
 export enum MESSAGES {
   CIUDADNOTFOUND = 'La ciudad con este id no fue encontrada',
   CIUDADNOASSOCIATEDSUPERMERCADO = 'La ciudad con este id no esta asociada a este Supermercado',
-  SUPERMERCADONOTFOUND = 'El supermercado este id no fue encontrado',
+  SUPERMERCADONOTFOUND = 'El supermercado con este id no fue encontrado',
   SUPERMERCADONOASSOCIATECIUDAD = 'El supermercado con este id no esta asociado a esta ciudad'
 }
--- src/supermercado/supermercado.entity.ts
+++ src/supermercado/supermercado.entity.ts
@@ -31,5 +31,4 @@ export class SupermercadoEntity {
   @JoinTable()
   ciudades: CiudadEntity[];
 
-
 }
